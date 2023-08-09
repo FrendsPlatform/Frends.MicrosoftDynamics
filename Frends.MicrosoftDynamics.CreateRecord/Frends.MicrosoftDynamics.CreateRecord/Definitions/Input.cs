5e571d26b9859eb422b5e8ddef98021e301f30e7
ttossavainen <teemu.tossavainen@hiq.fi>
2023-08-09 10:31:48 +0300

    init

---
+++ Frends.MicrosoftDynamics.CreateRecord/Frends.MicrosoftDynamics.CreateRecord/Definitions/Input.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Frends.MicrosoftDynamics.CreateRecord.Definitions;
+
+/// <summary>
+/// Input parameters.
+/// </summary>
+public class Input
+{
+    /// <summary>
+    /// Dataverse Connection String.
+    /// </summary>
+    /// <example>AuthType=Office365;Username=jsmith @contoso.onmicrosoft.com;Password=passcode;Url=https://contoso.crm.dynamics.com</example>
+    [PasswordPropertyText]
+    [DisplayFormat(DataFormatString = "Text")]
+    public string ConnectionString { get; set; }
+
+    /// <summary>
+    /// Entity logical name in Dynamics 365.
+    /// </summary>
+    /// <example>contacts</example>
+    public string EntityLogicalName { get; set; }
+
+    /// <summary>
+    /// Entity parameters.
+    /// </summary>
+    /// <example>[
+    /// { Name = "first_name", Value = "John" },
+    /// { Name = "last_name", Value = "Doe" }
+    /// ]</example>
+    public EntityParameter[] Parameters { get; set; }
+}
+
+/// <summary>
+/// Procedure parameter.
+/// </summary>
+public class EntityParameter
+{
+    /// <summary>
+    /// The name of the parameter.
+    /// </summary>
+    /// <example>first_name</example>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The value of the parameter.
+    /// </summary>
+    /// <example>FirstName</example>
+    public string Value { get; set; }
+}
---
+++ Frends.MicrosoftDynamics.CreateRecord/README.md
@@ -0,0 +1,26 @@
+# Frends.MicrosoftDynamics.CreateRecord
+Frends Task to create record to Microsoft Dynamics 365.
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT) 
+[![Build](https://github.com/FrendsPlatform/Frends.MicrosoftDynamics/actions/workflows/CreateRecord_build_and_test_on_main.yml/badge.svg)](https://github.com/FrendsPlatform/Frends.MicrosoftDynamics/actions)
+![Coverage](https://app-github-custom-badges.azurewebsites.net/Badge?key=FrendsPlatform/Frends.MicrosoftDynamics/Frends.MicrosoftDynamics.CreateRecord|main)
+
+# Installing
+
+You can install the Task via Frends UI Task View.
+
+## Building
+
+
+Rebuild the project
+
+`dotnet build`
+
+Run tests
+
+`dotnet test`
+
+
+Create a NuGet package
+
+`dotnet pack --configuration Release`
